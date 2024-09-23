1e232676b036c45ea5251622aaca5b0f274bce34
Aitor047 <aitorbronch@gmail.com>
2024-09-23 19:49:33 +0200

    Ejercicios 1 y 2 Y 3

---
+++ EjerciciosStream/src/Ejercicio3/Main.java
@@ -0,0 +1,13 @@
+package Ejercicio3;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+        int sumaCuadraos = numeros.stream().map(n -> n * n).reduce(0, Integer::sum);
+        System.out.println(sumaCuadraos);
+    }
+}
