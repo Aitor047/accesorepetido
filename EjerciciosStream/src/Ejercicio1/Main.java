7f9c398248762edf9fba6ea2c0dc3a3ce6ece88b
Aitor047 <aitorbronch@gmail.com>
2024-09-23 19:41:52 +0200

    Ejercicios 1 y 2

--- EjerciciosStream/src/Ejercicio1/Main.java
+++ EjerciciosStream/src/Ejercicio1/Main.java
@@ -9,8 +9,9 @@ public class Main {
         Fruta pera = new Fruta("Verde", "Pera");
         Fruta mandarina = new Fruta("Naranja", "Mandarina");
         Fruta melon = new Fruta("Verde", "Melon");
-        List<String> fruteria = Arrays.asList();
-        //fruteria.stream().map(Fruta::getNombre).forEach(System.out::println);
+        List<Fruta> fruteria = Arrays.asList(naranja, pera, mandarina, melon);
+        fruteria.stream().map(Fruta::getNombre).forEach(System.out::println);
+
     }
 
 }
