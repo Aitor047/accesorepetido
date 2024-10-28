158ef38d12908a4805504d3e17d0932fa0171ef8
Aitor047 <aitorbronch@gmail.com>
2024-10-28 20:22:16 +0100

    nose

---
+++ ejemploExcepciones/ejemploExcepciones/src/main/java/org/iesch/ad/ejemploExcepciones/config/Configuracion.java
@@ -0,0 +1,26 @@
+package org.iesch.ad.ejemploExcepciones.config;
+
+import org.iesch.ad.ejemploExcepciones.model.Usuario;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class Configuracion {
+
+    List<Usuario> usuarios(){
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(Usuario.builder().id(1).nombre("Aitor").apellidos("Monzon").build());
+        usuarios.add(Usuario.builder().id(2).nombre("Mariano").apellidos("Blesa").build());
+        usuarios.add(Usuario.builder().id(3).nombre("Alberto").apellidos("Perez").build());
+        usuarios.add(Usuario.builder().id(4).nombre("Paco").apellidos("Rajoy").build());
+        usuarios.add(Usuario.builder().id(5).nombre("Pepe").apellidos("Abascal").build());
+        usuarios.add(Usuario.builder().id(6).nombre("Franco").apellidos("Sanchez").build());
+        usuarios.add(Usuario.builder().id(7).nombre("Francisco").apellidos("Errejón").build());
+        return usuarios;
+    }
+
+
+
+}
---
+++ ejercicio3/src/main/java/iesch/org/ejercicio3/IuApplication.java
@@ -0,0 +1,11 @@
+package iesch.org.ejercicio3;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class IuApplication  implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
+}
